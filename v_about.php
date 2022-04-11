7fa26ef706c8b93f4c6b61e643114e15fb81abe4
Ayupermatasari081201 <101980114+Ayupermatasari081201@users.noreply.github.com>
2022-04-11 09:57:32 +0630

    Add files via upload

---
+++ v_about.php
@@ -0,0 +1,23 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Ayu Permata</li>
+        <li>Nama Belakang : Sari</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Desa. Tenjojaya Kp. Kebon Kalapa Rt.02/03</li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Sukabumi</li>
+    </ul>
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Bulutangkis</li>
+        <li>Volly</li>
+    </ul>
+</section>
